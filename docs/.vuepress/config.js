dbeafdb4d264fec5276ad2d6893a522db7fed9f1
lhw5072061.00 <lihongwei0305>
2022-04-11 17:47:34 +0800

    配置端口,vite打包

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ const {navbar} = require('./configs/index')
 module.exports = {
     title: '度半半半',
     description: '我的个人网站',
+    port: 8998,
     head: [ // 注入到当前页面的 HTML <head> 中的标签
         ['link', {rel: 'icon', href: 'https://vuejs.org/images/logo.png'}], // 增加一个自定义的 favicon(网页标签的图标)
     ],
@@ -18,5 +19,7 @@ module.exports = {
 
         sidebar: 'auto', // 侧边栏配置
         sidebarDepth: 2, // 侧边栏显示2级
-    }
+    },
+    bundler: '@vuepress/bundler-vite',
+
 };
